62c17e7f310af0239698351d23bb36f9f29a3586
stdnullptr <antonio.liubchev@gmail.com>
2025-01-28 23:06:17 +0200

    Initial commit

---
+++ src/main/java/com/stdnullptr/tiktokdownloaderservice/Application.java
@@ -0,0 +1,13 @@
+package com.stdnullptr.tiktokdownloaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokDownloaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiktokDownloaderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stdnullptr/tiktokdownloaderservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdnullptr.tiktokdownloaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokDownloaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
